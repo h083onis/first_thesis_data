042c29c296b095c1f7c710cf46f8a14857535b3e
onishi <h520083m@mails.cc.ehime-u.ac.jp>
2023-06-23 16:19:15 +0900

    パイプラインを差分行に対して柔軟に取得できるように改良した

--- source2/code/exe_make_snippet.py
+++ source2/code/exe_make_snippet.py
@@ -7,14 +7,13 @@ def main():
     repo_name = repo_path.split('/')[-1]
     line_length = sys.argv[2]
     pyfile ='pipeline_spiral3.py'
-    
-    for i in range(0, int(line_length)+1):
-        cnt_srcfile = 'cnt_source_file_'+repo_name+'_'+str(i)+'.txt'
-        hexsha_filename = 'hexsha_filename_'+repo_name+'_'+str(i)+'.txt'
-        error_log = 'error_log_'+repo_name+'_'+str(i)+'.txt'
-        snippet = 'snippet_'+repo_name+'_'+str(i)+'.txt'
-        process = subprocess.Popen(['python', pyfile, repo_path, str(i), cnt_srcfile,hexsha_filename, error_log, snippet])
-        process.wait()
+    i=line_length
+    cnt_srcfile = 'cnt_source_file_'+repo_name+'_'+str(i)+'.txt'
+    hexsha_filename = 'hexsha_filename_'+repo_name+'_'+str(i)+'.txt'
+    error_log = 'error_log_'+repo_name+'_'+str(i)+'.txt'
+    snippet = 'snippet_'+repo_name+'_'+str(i)+'.txt'
+    process = subprocess.Popen(['python', pyfile, repo_path, str(i), cnt_srcfile,hexsha_filename, error_log, snippet])
+    process.wait()
 
     sys.exit(0)
     
