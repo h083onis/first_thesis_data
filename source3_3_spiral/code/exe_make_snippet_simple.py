b1de6215ab1c6fafead76f5434626aa2789cb4be
onishi <h520083m@mails.cc.ehime-u.ac.jp>
2023-07-10 13:13:04 +0900

    文脈25トークン行まで取得

--- source3_3_spiral/code/exe_make_snippet_simple.py
+++ source3_3_spiral/code/exe_make_snippet_simple.py
@@ -6,7 +6,7 @@ def main():
     repo_path = sys.argv[1]
     repo_name = repo_path.split('/')[-1]
     diff_range = sys.argv[2]
-    pyfile ='pipeline_spiral_simple.py'
+    pyfile ='pipeline_simple.py'
     
     snippet = 'snippet_'+repo_name
     process = subprocess.Popen(['python', pyfile, repo_path, diff_range, snippet])
